aa62b415e04a37c26ea614defa1a56a892b932b6
sjmitch-git <sjmitch20@outlook.com>
2025-06-02 11:08:59 +0100

    Adds temp home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@/styles/index.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,8 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className="bg-primary">
+        <main className="flex-grow container mx-auto max-w-4xl pt-12 pb-12 px-0">{children}</main>
       </body>
     </html>
   );
---
+++ tailwind.config.ts
@@ -0,0 +1,72 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@smitch/fluid/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          DEFAULT: "var(--light)",
+        },
+        dark: {
+          DEFAULT: "var(--dark)",
+        },
+        primary: {
+          DEFAULT: "var(--primary)",
+          light: "var(--primary-light)",
+          dark: "var(--primary-dark)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          light: "var(--secondary-light)",
+          dark: "var(--secondary-dark)",
+        },
+        accent: {
+          DEFAULT: "var(--accent)",
+          light: "var(--accent-light)",
+          dark: "var(--accent-dark)",
+        },
+        neutral: {
+          DEFAULT: "var(--neutral)",
+          light: "var(--neutral-light)",
+          dark: "var(--neutral-dark)",
+        },
+        info: {
+          DEFAULT: "var(--info)",
+          light: "var(--info-light)",
+          dark: "var(--info-dark)",
+        },
+        success: {
+          DEFAULT: "var(--success)",
+          light: "var(--success-light)",
+          dark: "var(--success-dark)",
+        },
+        warning: {
+          DEFAULT: "var(--warning)",
+          light: "var(--warning-light)",
+          dark: "var(--warning-dark)",
+        },
+        error: {
+          DEFAULT: "var(--error)",
+          light: "var(--error-light)",
+          dark: "var(--error-dark)",
+        },
+        danger: {
+          DEFAULT: "var(--danger)",
+          light: "var(--danger-light)",
+          dark: "var(--danger-dark)",
+        },
+      },
+    },
+  },
+  /*  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+  ], */
+} satisfies Config;
