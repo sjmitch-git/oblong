c25605da562ff6ef35dc628871301052066363d4
sjmitch-git <sjmitch20@outlook.com>
2025-06-02 11:15:10 +0100

    Adds temp home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <article className="flex flex-col items-center justify-center min-h-50 p-6 text-light space-y-8">
-      <h1 className="text-3xl font-bold">We&apos;ll Be Up Soon!</h1>
+      <h1 className="text-3xl font-bold uppercase">We&apos;ll Be Up Soon!</h1>
       <p className="text-lg mb-2">Our website is currently under construction.</p>
       <p className="text-base">Thank you for your patience. Please check back later.</p>
     </article>
